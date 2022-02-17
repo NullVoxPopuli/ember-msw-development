80b504c0a7859a01dee78fa2e9b6d1a56a61fed6
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-17 16:03:52 -0500

    Fetch data and render it when available (right now this'll 404)

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { registerDestructor } from '@ember/destroyable';
 import { DEBUG } from '@glimmer/env';
 
 import { setupWorker } from 'msw';
@@ -7,11 +8,25 @@ export default class ApplicationRoute extends Route {
   async beforeModel() {
     // Don't include MSW in production, only in DEBUG (tests, development)
     if (DEBUG) {
-      let worker = setupWorker();
-      await worker.start();
-
-      //
-      worker.printHandlers();
+      await setupMSW(this);
     }
   }
+
+  async model() {
+    let response = await fetch('/blogs');
+    let json = response.json();
+
+    return { blogs: json };
+  }
+}
+
+async function setupMSW(context) {
+  let worker = setupWorker();
+  await worker.start();
+
+  worker.printHandlers();
+
+  // Prevent duplication in tests,
+  // where the app is setup and torn down a lot
+  registerDestructor(context, () => worker.stop());
 }
